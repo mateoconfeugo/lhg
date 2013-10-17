3d0ba65f95355a6e4b22bf0fcd94d2bc50230a0c
Matt Burns <matthewburns@gmail.com>
2013-10-17 15:00:49 -0700

    changed uberjar name and min lein version

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject lhg "0.1.0-SNAPSHOT"
+(defproject lhg "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :heroku {:app-name "lhg" :app-url "http://lhg.herokuapp.com"}
   :ring {:handler lhg.core/webapp :auto-reload? true :auto-refresh true}
+  :uberjar-name "lhg-standalone.jar"
+  :min-lein-version "2.0.0"
   :main lhg.core
   :dependencies [[org.clojure/clojure "1.5.1"] ; Lisp On JVM
                  [compojure "1.1.5"] ; Web routing]
