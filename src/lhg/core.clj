45c5d77081ae7e5539b58e919f242a7f860875c9
Matt Burns <matthewburns@gmail.com>
2013-10-18 13:54:33 -0700

    simplest thing

--- src/lhg/core.clj
+++ src/lhg/core.clj
@@ -5,6 +5,7 @@
   (:require [compojure.handler :as handler :refer [site]])
   (:gen-class))
 
+;; routes
 (defroutes app-routes (GET "/" [] "<h1>lhg server is up</h1>"))
 (def webapp (handler/site app-routes))
 
